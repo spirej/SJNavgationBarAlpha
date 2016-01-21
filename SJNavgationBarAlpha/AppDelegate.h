970864994cb4c7073c7327b04f6399dae00f4e4b
fushengjun <fushengjun@xiangshang360.com>
2016-01-21 14:34:33 +0800

    Initial Commit

---
+++ SJNavgationBarAlpha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJNavgationBarAlpha
+//
+//  Created by SPIREJ on 16/1/21.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJNavgationBarAlpha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJNavgationBarAlpha
+//
+//  Created by SPIREJ on 16/1/21.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
